c92fd90d8ba21eaf9dea79000bc886160c0f377a
Quique1409 <enriquemedranorabak@hotmail.com>
2025-07-02 18:52:38 -0600

    Se crea una nueva función en vuelo3.py esta hace que vaya hacia adelante y regrese a su posición

--- prueba_vuelos/vuelo3.py
+++ prueba_vuelos/vuelo3.py
@@ -5,7 +5,7 @@ from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.log import LogConfig
+#from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
@@ -13,11 +13,18 @@ from cflib.utils import uri_helper
 
 URI = uri_helper.uri_from_env(default='radio://0/70/2M/E7E7E7E7E5')
 DEFAULT_HEIGHT = 0.5
-BOX_LIMIT = 0.5
+#BOX_LIMIT = 0.5
 
 deck_attached_event = Event()
 
 logging.basicConfig(level=logging.ERROR)
+
+#funtion takeoff
+def take_off_simple(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        #mc.up(0.3)
+        time.sleep(3)
+        mc.stop()
 
 
 def param_deck_flow(_, value_str):
@@ -29,18 +36,20 @@ def param_deck_flow(_, value_str):
     else:
         print('Deck is not attached')
 
-#funtion takeoff
-def take_off_simple(scf):
+#funtion move_linear
+def move_linear_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        mc.up(0.3)
-        time.sleep(3)
-        mc.stop()
+        time.sleep(1)
+        mc.forward(0.5)
+        time.sleep(1)
+        mc.back(0.5)
+        time.sleep(1)
 
 if __name__ == '__main__':
 
     cflib.crtp.init_drivers()
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-        scf.cf.param.add_update_callback(group="deck", name="bcFlow", cb=param_deck_flow)
+        scf.cf.param.add_update_callback(group="deck", name="bcFlow2", cb=take_off_simple)
         time.sleep(1)
 
         if not deck_attached_event.wait(timeout=5):
